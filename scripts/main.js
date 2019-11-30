e63724038addfc72ac4dbfb2f0c488f07f5519cf
Davina Leong <davina.leong@outlook.com>
2019-11-30 10:53:22 +0800

    Successfully appended color sets

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,20 @@
 $(document).ready(function () {
     $.getJSON('./../db/colors.json', function(colors) {
-        console.log(colors);
+        // console.log(colors);
 
         const main = $('main');
 
-        
+        colors.forEach(color => {
+            // console.log(color.set);
+            main.append(
+                '<section class="palette-set">' +
+                '\t<h2 class="palette-title">' + color.set + '</h2>' +
+                '\t' +
+                '\t\t<div class="palette-swatches">' +
+                '\t\t\t' +
+                '\t\t</div>' +
+                '</section>'
+            );
+        });
     });
 });
