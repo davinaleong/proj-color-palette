d8d9ea373e3cf9b83b3c63f45380751f45b017d6
Davina Leong <davina.leong@outlook.com>
2019-11-30 14:49:22 +0800

    Added nav for easy navigation

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,29 @@
 $(document).ready(function () {
-    const main = $('main');
 
     colors.forEach((set, index) => {
-        main.append(
-            '<section id="set-' + index + '" class="palette-set">' +
+        // Convert name to kebab-case
+        const name = {
+            original: set.name,
+            split: set.name.toLowerCase().split(' ')
+        };
+        name.kebab = '';
+        name.split.forEach((word, index) => {
+            if (index >= name.split.length - 1) {
+                name.kebab += word;
+            } else {
+                name.kebab += word + '-';
+            }
+        });
+
+        
+        // Append links to <nav>
+        $('ul.nav-links').append(
+            '\t<li><a href="#set-' + name.kebab + '">' + set.name + '</a></li>'
+        );
+
+        // Append palette set to <main>
+        $('main').append(
+            '<section id="set-' + name.kebab + '" class="palette-set">' +
             '\t<h2 class="palette-title">' + set.name + '</h2>' +
             '\t' +
             '\t\t<div id="set-' + index + '-swatches" class="palette-swatches">' +
@@ -12,6 +32,7 @@ $(document).ready(function () {
             '</section>'
         );
 
+        // Append swatches to palette set via id
         let swatches = '';
         set.colors.forEach(color => {
             swatches += '<div class="palette-swatch" style="color: ' + color.text + '; background-color: ' + color.value + '">' +
