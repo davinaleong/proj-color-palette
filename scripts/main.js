96a1226f5338b6f02595e9484af810ed3110eb8a
Davina Leong <davina.leong@outlook.com>
2019-11-30 11:05:30 +0800

    Appended color swatches

--- scripts/main.js
+++ scripts/main.js
@@ -1,20 +1,30 @@
 $(document).ready(function () {
-    $.getJSON('./../db/colors.json', function(colors) {
+    $.getJSON('./../db/colors.json', function(colorSets) {
         // console.log(colors);
 
         const main = $('main');
 
-        colors.forEach(color => {
-            // console.log(color.set);
+        colorSets.forEach((set, index) => {
+            console.log(set.name);
             main.append(
-                '<section class="palette-set">' +
-                '\t<h2 class="palette-title">' + color.set + '</h2>' +
+                '<section id="set-' + index + '" class="palette-set">' +
+                '\t<h2 class="palette-title">' + set.name + '</h2>' +
                 '\t' +
-                '\t\t<div class="palette-swatches">' +
+                '\t\t<div id="set-' + index + '-swatches" class="palette-swatches">' +
                 '\t\t\t' +
                 '\t\t</div>' +
                 '</section>'
             );
+
+            let swatches = '';
+            set.colors.forEach(color => {
+                console.log(color.name, color.value);
+                swatches += '<div class="palette-swatch text-black" style="background-color: ' + color.value + '">' +
+                '\t<p class="swatch-name">' + color.name + '</p>' +
+                '\t<p class="swatch-value">' + color.value + '</p>' +
+                '</div>';
+            })
+            $('#set-' + index + '-swatches').append(swatches);
         });
     });
 });
