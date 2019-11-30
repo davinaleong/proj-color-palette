194919147edfa76b110b6e37524a834f04ed3e09
Davina Leong <davina.leong@outlook.com>
2019-11-30 11:23:51 +0800

    Use a js object instead of a json file to provide color data

---
+++ scripts/db/colors.js
@@ -0,0 +1,121 @@
+const colors = [
+    {
+        "name": "Basic Colors",
+        "colors": [
+            {
+                "name": "White",
+                "value": "#ffffff",
+                "text": "#00000"
+            }, {
+                "name": "Black",
+                "value": "#000000",
+                "text": "#ffffff"
+            }, {
+                "name": "Gray",
+                "value": "#888888",
+                "text": "#00000"
+            }, {
+                "name": "Red",
+                "value": "#ff0000",
+                "text": "#ffffff"
+            }, {
+                "name": "Yellow",
+                "value": "#ffff00",
+                "text": "#00000"
+            }, {
+                "name": "Green",
+                "value": "#00ff00",
+                "text": "#00000"
+            }, {
+                "name": "Cyan",
+                "value": "#00ffff",
+                "text": "#00000"
+            }, {
+                "name": "Blue",
+                "value": "#0000ff",
+                "text": "#ffffff"
+            }, {
+                "name": "Magenta",
+                "value": "#ff00ff",
+                "text": "#ffffff"
+            }
+        ]
+    }, {
+        "name": "Pokémon Colors",
+        "colors": [
+            {
+                "name": "Bug",
+                "value": "#A8B821",
+                "text": "#00000"
+            }, {
+                "name": "Dark",
+                "value": "#6F5848",
+                "text": "#ffffff"
+            }, {
+                "name": "Dragon",
+                "value": "#7038F9",
+                "text": "#ffffff"
+            }, {
+                "name": "Electric",
+                "value": "#F8D030",
+                "text": "#00000"
+            }, {
+                "name": "Fairy",
+                "value": "#FFAEC9",
+                "text": "#00000"
+            }, {
+                "name": "Fighting",
+                "value": "#C03028",
+                "text": "#ffffff"
+            }, {
+                "name": "Fire",
+                "value": "#F07F2F",
+                "text": "#00000"
+            }, {
+                "name": "Flying",
+                "value": "#A890F0",
+                "text": "#00000"
+            }, {
+                "name": "Ghost",
+                "value": "#715899",
+                "text": "#ffffff"
+            }, {
+                "name": "Grass",
+                "value": "#78C750",
+                "text": "#00000"
+            }, {
+                "name": "Ground",
+                "value": "#DEBF67",
+                "text": "#00000"
+            }, {
+                "name": "Ice",
+                "value": "#98D8D8",
+                "text": "#00000"
+            }, {
+                "name": "Normal",
+                "value": "#A9A878",
+                "text": "#00000"
+            }, {
+                "name": "Poison",
+                "value": "#A040A1",
+                "text": "#ffffff"
+            }, {
+                "name": "Psychic",
+                "value": "#F85788",
+                "text": "#ffffff"
+            }, {
+                "name": "Rock",
+                "value": "#B89F38",
+                "text": "#ffffff"
+            }, {
+                "name": "Steel",
+                "value": "#B8B8D0",
+                "text": "#00000"
+            }, {
+                "name": "Water",
+                "value": "#6890F0",
+                "text": "#00000"
+            }
+        ]
+    }
+];
--- scripts/main.js
+++ scripts/main.js
@@ -1,30 +1,26 @@
 $(document).ready(function () {
-    $.getJSON('./../db/colors.json', function(colorSets) {
-        // console.log(colors);
+    const main = $('main');
 
-        const main = $('main');
+    colors.forEach((set, index) => {
+        console.log(set.name);
+        main.append(
+            '<section id="set-' + index + '" class="palette-set">' +
+            '\t<h2 class="palette-title">' + set.name + '</h2>' +
+            '\t' +
+            '\t\t<div id="set-' + index + '-swatches" class="palette-swatches">' +
+            '\t\t\t' +
+            '\t\t</div>' +
+            '</section>'
+        );
 
-        colorSets.forEach((set, index) => {
-            console.log(set.name);
-            main.append(
-                '<section id="set-' + index + '" class="palette-set">' +
-                '\t<h2 class="palette-title">' + set.name + '</h2>' +
-                '\t' +
-                '\t\t<div id="set-' + index + '-swatches" class="palette-swatches">' +
-                '\t\t\t' +
-                '\t\t</div>' +
-                '</section>'
-            );
-
-            let swatches = '';
-            set.colors.forEach(color => {
-                console.log(color.name, color.value);
-                swatches += '<div class="palette-swatch" style="color: ' + color.text + '; background-color: ' + color.value + '">' +
-                '\t<p class="swatch-name">' + color.name + '</p>' +
-                '\t<p class="swatch-value">' + color.value + '</p>' +
-                '</div>';
-            })
-            $('#set-' + index + '-swatches').append(swatches);
-        });
+        let swatches = '';
+        set.colors.forEach(color => {
+            console.log(color.name, color.value);
+            swatches += '<div class="palette-swatch" style="color: ' + color.text + '; background-color: ' + color.value + '">' +
+            '\t<p class="swatch-name">' + color.name + '</p>' +
+            '\t<p class="swatch-value">' + color.value + '</p>' +
+            '</div>';
+        })
+        $('#set-' + index + '-swatches').append(swatches);
     });
 });
