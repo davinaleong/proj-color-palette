5bc104b9a667d9ff45b88350180f1b1b37d79188
Davina Leong <davina.leong@outlook.com>
2019-11-30 11:49:39 +0800

    Added Bootstrap colors

--- scripts/db/colors.js
+++ scripts/db/colors.js
@@ -4,40 +4,40 @@ const colors = [
         "colors": [
             {
                 "name": "White",
-                "value": "#ffffff",
+                "value": "#FFFFF",
                 "text": "#00000"
             }, {
                 "name": "Black",
                 "value": "#000000",
-                "text": "#ffffff"
+                "text": "#FFFFFF"
             }, {
                 "name": "Gray",
                 "value": "#888888",
                 "text": "#00000"
             }, {
                 "name": "Red",
-                "value": "#ff0000",
-                "text": "#ffffff"
+                "value": "#FF0000",
+                "text": "#FFFFFF"
             }, {
                 "name": "Yellow",
-                "value": "#ffff00",
+                "value": "#FFFF00",
                 "text": "#00000"
             }, {
                 "name": "Green",
-                "value": "#00ff00",
+                "value": "#00FF00",
                 "text": "#00000"
             }, {
                 "name": "Cyan",
-                "value": "#00ffff",
+                "value": "#00FFFF",
                 "text": "#00000"
             }, {
                 "name": "Blue",
-                "value": "#0000ff",
-                "text": "#ffffff"
+                "value": "#0000FF",
+                "text": "#FFFFFF"
             }, {
                 "name": "Magenta",
-                "value": "#ff00ff",
-                "text": "#ffffff"
+                "value": "#FF00FF",
+                "text": "#FFFFFF"
             }
         ]
     }, {
@@ -50,11 +50,11 @@ const colors = [
             }, {
                 "name": "Dark",
                 "value": "#6F5848",
-                "text": "#ffffff"
+                "text": "#FFFFFF"
             }, {
                 "name": "Dragon",
                 "value": "#7038F9",
-                "text": "#ffffff"
+                "text": "#FFFFFF"
             }, {
                 "name": "Electric",
                 "value": "#F8D030",
@@ -66,7 +66,7 @@ const colors = [
             }, {
                 "name": "Fighting",
                 "value": "#C03028",
-                "text": "#ffffff"
+                "text": "#FFFFFF"
             }, {
                 "name": "Fire",
                 "value": "#F07F2F",
@@ -78,7 +78,7 @@ const colors = [
             }, {
                 "name": "Ghost",
                 "value": "#715899",
-                "text": "#ffffff"
+                "text": "#FFFFFF"
             }, {
                 "name": "Grass",
                 "value": "#78C750",
@@ -98,15 +98,15 @@ const colors = [
             }, {
                 "name": "Poison",
                 "value": "#A040A1",
-                "text": "#ffffff"
+                "text": "#FFFFFF"
             }, {
                 "name": "Psychic",
                 "value": "#F85788",
-                "text": "#ffffff"
+                "text": "#FFFFFF"
             }, {
                 "name": "Rock",
                 "value": "#B89F38",
-                "text": "#ffffff"
+                "text": "#FFFFFF"
             }, {
                 "name": "Steel",
                 "value": "#B8B8D0",
@@ -115,6 +115,87 @@ const colors = [
                 "name": "Water",
                 "value": "#6890F0",
                 "text": "#00000"
+            }
+        ]
+    }, {
+        "name": "Bootstrap Colors",
+        "colors": [
+            {
+                "name": "Blue (Primary)",
+                "value": "#007BFF",
+                "text": "#FFFFFF"
+            }, {
+                "name": "Indigo",
+                "value": "#6610F2",
+                "text": "#FFFFFF"
+            }, {
+                "name": "Purple",
+                "value": "#6F42C1",
+                "text": "#FFFFFF"
+            }, {
+                "name": "Pink",
+                "value": "#E83E8C",
+                "text": "#FFFFFF"
+            }, {
+                "name": "Red (Danger)",
+                "value": "#DC3545",
+                "text": "#FFFFFF"
+            }, {
+                "name": "Orange",
+                "value": "#FD7E14",
+                "text": "#000000"
+            }, {
+                "name": "Yellow (Warning)",
+                "value": "#FFC107",
+                "text": "#000000"
+            }, {
+                "name": "Green (Success)",
+                "value": "#28A745",
+                "text": "#000000"
+            }, {
+                "name": "Teal",
+                "value": "#20C997",
+                "text": "#000000"
+            }, {
+                "name": "Cyan (Info)",
+                "value": "#17A2B8",
+                "text": "#FFFFFF"
+            }, {
+                "name": "Gray-100 (Light)",
+                "value": "#F8F9FA",
+                "text": "#000000"
+            }, {
+                "name": "Gray-200",
+                "value": "#E9ECEF",
+                "text": "#000000"
+            }, {
+                "name": "Gray-300",
+                "value": "#DEE2E6",
+                "text": "#000000"
+            }, {
+                "name": "Gray-400",
+                "value": "#CED4DA",
+                "text": "#000000"
+            }, {
+                "name": "Gray-500",
+                "value": "#ADB5BD",
+                "text": "#000000"
+            }, {
+                "name": "Gray-600 (Secondary)",
+                "value": "#6C757D",
+                "text": "#FFFFFF"
+            }, {
+                "name": "Gray-700",
+                "value": "#495057",
+                "text": "#FFFFFF"
+            }, {
+                "name": "Gray-800",
+                "value": "#343A40",
+                "text": "#FFFFFF"
+            }, {
+                "name": "Gray-900 (Dark)",
+                "value": "#212529",
+                "text": "#FFFFFF"
             }
         ]
     }
